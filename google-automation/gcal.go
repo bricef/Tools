3663653efc21d22fa6e43f7c6792c26c7e94660b
Brice Fernandes <brice.fernandes@raspberrypi.org>
2025-08-13 17:44:30 +0100

    Improve date and formatting for gcal tool

--- google-automation/gcal.go
+++ google-automation/gcal.go
@@ -116,11 +116,11 @@ type Event struct {
 	Type    string
 }
 
-func getEvents(srv *calendar.Service) []Event {
+func getEvents(srv *calendar.Service, start time.Time, end time.Time) []Event {
 	events := make([]Event, 0)
 
-	t := time.Now().Format(time.RFC3339)
-	t2 := time.Now().AddDate(0, 0, 10).Format(time.RFC3339)
+	t := start.Format(time.RFC3339)
+	t2 := end.Format(time.RFC3339)
 
 	items, err := srv.Events.List("primary").
 		ShowDeleted(false).
@@ -166,14 +166,23 @@ func getEvents(srv *calendar.Service) []Event {
 	return events
 }
 
+func beginning_of_day(t time.Time) time.Time {
+	year, month, day := t.Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+}
+
 func main() {
 	srv := getCalendarService()
-	events := getEvents(srv)
+	start := beginning_of_day(time.Now())
+	end := start.AddDate(0, 0, 7)
+	events := getEvents(srv, start, end)
 
 	if len(events) == 0 {
 		fmt.Println("No upcoming events found.")
 	} else {
 		currentDay := time.Now()
+		fmt.Printf("\n### %v\n\n", currentDay.Format("Monday 02"))
+
 		for _, event := range events {
 			if event.Type == "focusTime" ||
 				event.Type == "workingLocation" ||
